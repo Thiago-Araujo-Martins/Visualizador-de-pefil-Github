a8daca192372584df905cef8b480fc21cd4507de
Thiago-Araujo-Martins <149716826+Thiago-Araujo-Martins@users.noreply.github.com>
2026-04-26 20:47:11 -0300

    adiciona verificação caso não tenha um nome

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -20,7 +20,7 @@ export function renderProfile(userData, userRepos, container) {
     userData.name
   }" class="profile-avatar">
       <div class="profile-info">
-        <h2>${userData.name}</h2>
+        <h2>${userData.name || "não possui nome cadastrado 😢"}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
       </div>
     </div>
